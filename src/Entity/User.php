ceaeefcea641eb7e7bd09fdbb5d03e8d053bae17
regazzoniam <amandineregad@gmail.com>
2022-09-14 09:11:40 +0200

    update template

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -350,4 +350,10 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    
+    // public function isAdmin(): bool
+    // {
+    //     return in_array('ROLE_ADMIN', $this->getRoles());
+    // }
 }
